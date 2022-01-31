05fd34793468ff4f38c11827b22bf35ed7193d4f
ZubilyaM <98767070+ZubilyaM@users.noreply.github.com>
2022-01-31 23:21:23 +0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+ function fun() {
+   var num1 = document.getElementById('number').value;
+   var num2 = document.getElementById('numbers').value;
+   document.getElementById('result').innerHTML = (num1 * num2)
+ }
+
+ function fun2() {
+   var num1 = document.getElementById('number').value;
+   var num2 = document.getElementById('numbers').value;
+   document.getElementById('result').innerHTML = (num1 / num2)
+ }
